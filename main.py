dd5af29e5908a58187b7c1451a9b4a543932d0a1
Luka Dolgov <luka.dolgov@gmail.com>
2024-05-28 17:35:24 -0400

    new files

--- main.py
+++ main.py
@@ -8,22 +8,30 @@ pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Bernouli sim')
 clock = pygame.time.Clock()
-test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
+test_font = pygame.font.Font('Pixeltype.ttf', 50)
 start_time = 0
 
 
 pipe = pygame.sprite.GroupSingle()
-pipe.add(Pipe())
+#pipe.add(Pipe())
 molecule_group = pygame.sprite.Group()
+
+molecule_timer = pygame.USEREVENT + 1
+pygame.time.set_timer(molecule_timer, 1500)
+
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit() 
+      #  if event.type == molecule_timer:
+           # molecule_group.add(Molecule())
     screen.fill('lightblue')
     pipe.draw(screen)
     pipe.update()
+    molecule_group.draw(screen)
+    molecule_group.update()
 
 
 
