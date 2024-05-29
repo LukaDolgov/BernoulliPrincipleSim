940f1881cbda408445436800f55733c4a26e0b75
Luka Dolgov <luka.dolgov@gmail.com>
2024-05-29 15:31:33 -0400

    nice

--- main.py
+++ main.py
@@ -36,6 +36,7 @@ class Pipe(pygame.sprite.Sprite):
 
         # Set the position of the rect to be centered on the start_pos
         self.rect.midleft = start_pos
+    
 
 class Molecule(pygame.sprite.Sprite):
     def __init__(self, velocity):
@@ -44,12 +45,20 @@ class Molecule(pygame.sprite.Sprite):
        self.xpos = startXpos
        self.color = "Blue"
        self.cubelength = 10
-       self.ypos = randint(130 + 20, 130 + distance_within - self.cubelength)
-       self.image = pygame.Surface((10, 10))
+       self.ypos = randint(start_point_Y_coord + self.cubelength, start_point_Y_coord + distance_within - self.cubelength)
+       self.image = pygame.Surface((self.cubelength, self.cubelength))
+       self.image.fill(self.color)
        self.image = self.image.convert_alpha()
        self.rect = self.image.get_rect()
        self.rect.centerx = startXpos
        self.rect.centery = self.ypos
+    def update(self):
+       self.rect.x += self.velocity
+       self.destroy()    
+    def destroy(self):
+        if self.rect.x >= 530:
+            self.kill()
+        
      
 
 
@@ -62,19 +71,24 @@ test_font = pygame.font.Font('Pixeltype.ttf', 50)
 start_time = 0
 
 #parameters
-molecular_velocity = 0
+molecular_velocity = 4
 distance_within = 50
+Area = (distance_within / 2) ** 2 * math.pi
 initial_pressure = 0
+flowrate = Area * molecular_velocity
 startXpos = 230
+start_point_X_coord = 230
+end_point_X_coord = 530
+start_point_Y_coord = 130
 
 #objects
 pipe = pygame.sprite.Group()
-pipe.add(Pipe((230, 130), (530, 130), "black"))
-pipe.add(Pipe((230, 130 + distance_within), (530, 130 + distance_within), "black"))
+pipe.add(Pipe((start_point_X_coord, start_point_Y_coord), (end_point_X_coord, start_point_Y_coord), "black"))
+pipe.add(Pipe((start_point_X_coord, start_point_Y_coord + distance_within), (end_point_X_coord, start_point_Y_coord + distance_within), "black"))
 molecule_group = pygame.sprite.Group()
 
 molecule_timer = pygame.USEREVENT + 1
-pygame.time.set_timer(molecule_timer, 1500)
+pygame.time.set_timer(molecule_timer, 200)
 
 #text
 sim_name = test_font.render('Bernoulli\'s Equation: ', False, 'blue')
@@ -92,7 +106,7 @@ while True:
         if event.type == pygame.MOUSEBUTTONDOWN:
             return_pos()
         if event.type == molecule_timer:
-           molecule_group.add(Molecule(1))
+           molecule_group.add(Molecule(molecular_velocity))
     screen.fill('lightblue')
     screen.blit(sim_name, sim_name_rect)
     pipe.draw(screen)
